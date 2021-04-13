52ab370a8e42defbf8f14078344b1485a12b6157
Pietros Balbino <pietros.balbino@gmail.com>
2021-04-13 18:34:27 -0300

    Teste - Remover

---
+++ Aula2TrilhaDotNet.Teste/Builder/RemoverSerraCircularRequestBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Aula2TrilhaDotNet.DTO.SerraCircular.RemoverSerraCircular;
+using Bogus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula2TrilhaDotNet.Teste.Builder {
+    class RemoverSerraCircularRequestBuilder {
+        private readonly Faker _faker = new Faker("pt_BR");
+        private readonly RemoverSerraCircularRequest _removerSerraCircularRequest;
+
+        public RemoverSerraCircularRequestBuilder() {
+
+            _removerSerraCircularRequest = new RemoverSerraCircularRequest();
+            _removerSerraCircularRequest.id = _faker.Random.Int(1,100);
+        }
+                
+        public RemoverSerraCircularRequest Builder() {
+            return _removerSerraCircularRequest;
+        }
+    }
+}
---
+++ Aula2TrilhaDotNet.Teste/UseCase/RemoverSerraCircularUseCaseTest.cs
@@ -0,0 +1,48 @@
+﻿using Aula2TrilhaDotNet.DTO.SerraCircular.RemoverSerraCircular;
+using Aula2TrilhaDotNet.Entities;
+using Aula2TrilhaDotNet.Repositorio;
+using Aula2TrilhaDotNet.Teste.Builder;
+using Aula2TrilhaDotNet.UseCase;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Aula2TrilhaDotNet.Teste.UseCase {
+    public class RemoverSerraCircularUseCaseTest {
+
+        private readonly Mock<IRepositorioSerraCircular> _repositorioSerraCircular;
+        private readonly RemoverSerraCircularUseCase _removerSerraCircularUseCase;
+
+        public RemoverSerraCircularUseCaseTest() {
+            _repositorioSerraCircular = new Mock<IRepositorioSerraCircular>();            
+            _removerSerraCircularUseCase = new RemoverSerraCircularUseCase(_repositorioSerraCircular.Object);
+        }
+
+        [Fact]
+        public void SerraCircular_Remover_QuandoRetornarSucesso() {
+            var response = new RemoverSerraCircularResponse();
+            var request = new RemoverSerraCircularRequestBuilder().Builder();
+           response.msg = "Removido com sucesso";
+
+            _repositorioSerraCircular.Setup(repositorio => repositorio.Remove(request.id)); ;
+
+            var resulte = _removerSerraCircularUseCase.Executar(request);
+            response.Should().BeEquivalentTo(resulte);
+        }
+
+        [Fact]
+        public void SerraCircular_QuandoRepositorioExcecao() {
+            var response = new RemoverSerraCircularResponse();
+            var request = new RemoverSerraCircularRequestBuilder().Builder();
+            response.msg = "Não foi possível remover a Serra Circular";
+
+            _repositorioSerraCircular.Setup(repositorio => repositorio.Remove(request.id)).Throws(new Exception());
+
+            var resulte = _removerSerraCircularUseCase.Executar(request);
+            response.Should().BeEquivalentTo(resulte);
+        }
+    }
+}
