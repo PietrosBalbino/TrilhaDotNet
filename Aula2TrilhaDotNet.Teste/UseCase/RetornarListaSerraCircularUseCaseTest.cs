4d0e833e6e54e986e6cc798c8f5d6d5a346a19a7
Pietros Balbino <pietros.balbino@gmail.com>
2021-04-13 19:41:26 -0300

    Teste - Retornar Lista

---
+++ Aula2TrilhaDotNet.Teste/UseCase/RetornarListaSerraCircularUseCaseTest.cs
@@ -0,0 +1,48 @@
+﻿using Aula2TrilhaDotNet.DTO.SerraCircular.RetornarListaSerraCircular;
+using Aula2TrilhaDotNet.Entities;
+using Aula2TrilhaDotNet.Repositorio;
+using Aula2TrilhaDotNet.Teste.Builder;
+using Aula2TrilhaDotNet.UseCase;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Aula2TrilhaDotNet.Teste.UseCase {
+    public class RetornarListaSerraCircularUseCaseTest {
+
+        private readonly Mock<IRepositorioSerraCircular> _repositorioSerraCircular;
+        private readonly RetornarListaSerraCircularUseCase _retornarListaSerraCircularUseCase;
+
+        public RetornarListaSerraCircularUseCaseTest() {
+            _repositorioSerraCircular = new Mock<IRepositorioSerraCircular>();
+            _retornarListaSerraCircularUseCase = new RetornarListaSerraCircularUseCase(_repositorioSerraCircular.Object);
+        }
+
+        [Fact]
+        public void SerraCircular_Remover_QuandoRetornarSucesso() {
+            var response = new RetornarListaSerraCircularResponse();
+            var serraCircular = new SerraCircular();
+            response.msg = "Lista de Serra Circular retornada com sucesso";
+
+            _repositorioSerraCircular.Setup(repositorio => repositorio.Listar()); ;
+
+            var resulte = _retornarListaSerraCircularUseCase.Executar();
+            response.Should().BeEquivalentTo(resulte);
+        }
+
+        [Fact]
+        public void SerraCircular_Remover_QuandoRepositorioExcecao() {
+            var response = new RetornarListaSerraCircularResponse();
+            var serraCircular = new SerraCircular();
+            response.msg = "Não foi possível retornar a lista de Serra Circular";
+
+            _repositorioSerraCircular.Setup(repositorio => repositorio.Listar()).Throws(new Exception()); ;
+
+            var resulte = _retornarListaSerraCircularUseCase.Executar();
+            response.Should().BeEquivalentTo(resulte);
+        }
+    }
+}
